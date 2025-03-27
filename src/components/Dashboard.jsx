04e80f827340b689321bc4514ceecb8449c8fcff
diekodavidd <adiekodavid@gmail.com>
2025-03-27 11:38:46 +0100

    soo

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,46 +1,71 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './style.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from "../assets/IMG_1460.PNG"; // Ensure the logo image is in the correct path
-import logor from "../assets/signin-image.jpg"; // Ensure the logo image is in the correct path
 import './logi.css';
 import BouncingBalls from "./BouncingBalls";
-import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import LockIcon from "@mui/icons-material/Lock";
-import EmailIcon from "@mui/icons-material/Email";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-
 
 const Dashboard = () => {
 
-     // Safely retrieve existingCustomer from localStorage
-  const storedCustomer = localStorage.getItem("existingCustomer")
-  ? JSON.parse(localStorage.getItem("existingCustomer"))
-  : null;
 
-    return (
-        <div>
-  <div style={{ position: "relative", width: "100vw", height: "100vh", overflow: "hidden" }}>
-    {/* Bouncing Balls Canvas */}
-    <BouncingBalls />
-    <Link to='/'><img 
-                src={logo} 
-                alt="Logo" 
-                style={{ position: "absolute", top: "20px", left: "20px", width: "100px", height: "100px" }}
+  // Safely retrieve existingCustomer from localStorage
+  // const storedCustomer = localStorage.getItem("existingCustomer")
+  //   ? JSON.parse(localStorage.getItem("existingCustomer"))
+  //   : null;
+
+  // Safely retrieve existingToke from localStorage
+  let storedToke = localStorage.getItem("existingToke")
+  const navigate = useNavigate()
+  const [data,setData] = useState()
+  useEffect(() => {
+
+    axios.get("http://localhost:7000/customer/verify",{
+      headers:{
+        'Authorization': `Bearer ${storedToke}`
+      }
+  })
+    .then((response=>{
+      console.log(response.data.User);
+      setData(response?.data?.User)
+
+    })).catch((err)=>{
+      console.log(err?.response?.data?.message);
+      navigate("/login"); // Redirect to login page
+    })
+    // if (!storedToke) {
+    //   navigate("/login"); // Redirect to login page
+    // }
+  }, [])
+
+  return (
+    <div>
+      <div style={{ position: "relative", width: "100vw", height: "100vh", overflow: "hidden" }}>
+        {/* Bouncing Balls Canvas */}
+        <BouncingBalls />
+        <Link to='/'><img
+          src={logo}
+          alt="Logo"
+          style={{ position: "absolute", top: "20px", left: "20px", width: "100px", height: "100px" }}
         /></Link>
-    
-    {/* Signi Form */}
-    <div className='freedo'><div>
-     <h1>Welcome {storedCustomer ? storedCustomer.firstname : "Guest"}!</h1>
-</div>
-      
-    </div>
-  </div>
-
-            
+         <div style={{ color: "white", position:"fixed", top:"20px" ,right:"20px", fontSize:"12px", display:"flex"   }}>
+          <AccountCircleIcon style={{fontSize:"35px" , color:"gold"  }} />
+         {/* <h1 style={{fontSize:"29px" , marginLeft:"5px"  }}>Welcome {storedCustomer ? storedCustomer.firstname : "Guest"}!</h1> */}
+         <h1 style={{fontSize:"29px" , marginLeft:"5px"  }}>Welcome {data ? data.firstname : "Guest"}!</h1>
+          
+         </div>
+        {/* Signi Form */}
+        <div className='freedo'><div>
+          {/* <h1 style={{ color: "white" }}>Welcome {storedCustomers ? storedCustomers.firstname : "Guest"}!</h1> */}
         </div>
-    );
+
+        </div>
+      </div>
+
+
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,10 +7,8 @@ import logo from "../assets/IMG_1460.PNG"; // Ensure the logo image is in the co
 import logor from "../assets/signin-image.jpg"; // Ensure the logo image is in the correct path
 import './logi.css';
 import BouncingBalls from "./BouncingBalls";
-import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import LockIcon from "@mui/icons-material/Lock";
 import EmailIcon from "@mui/icons-material/Email";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 
 const Login = () => {
@@ -18,27 +16,28 @@ const Login = () => {
         email: "",
         Password: ""
     });
-    
+
     const navigate = useNavigate(); // Hook to navigate to other pages
     const [loading, setLoading] = useState(false); // Add loading state
-    
+
 
     const LoginCustomer = async () => {
         try {
             setLoading(true);
-    
+
             const response = await axios.post(
-                "https://backend-details-0xik.onrender.com/customer/login",
+                "http://localhost:7000/customer/login",
                 userDetails
             );
-    
+
             console.log(response.data);
             console.log(response.data.existingCustomer);
 
             let damm = response.data.existingCustomer
-    
-            
-            localStorage.setItem("existingCustomer", JSON.stringify(damm));
+            let damme = response.data.token
+
+            // localStorage.setItem("existingCustomer", JSON.stringify(damm));//save the user info in existingCustomer to local storage
+            localStorage.setItem("existingToke", damme);//save the user token in respose to local storage
             if (response.data.status) {
                 alert("Login successful");
                 navigate("/dash"); // Redirect to login page
@@ -53,83 +52,83 @@ const Login = () => {
             setLoading(false);
         }
     };
-    
-    
+
+
 
     return (
         <div>
-  <div style={{ position: "relative", width: "100vw", height: "100vh", overflow: "hidden" }}>
-    {/* Bouncing Balls Canvas */}
-    <BouncingBalls />
-    <Link to='/'><img 
-                src={logo} 
-                alt="Logo" 
-                style={{ position: "absolute", top: "20px", left: "20px", width: "100px", height: "100px" }}
-        /></Link>
-    
-    {/* Signi Form */}
-    <div className='freedo'>
-     
-        <section className="signin">
-            <div className="container">
-                <div className="signin-content">
-                    <div className="signin-image">
-                        <figure><img src={logor} alt="sing up image"/></figure>
-                        <Link  className="signup-image-link" to='/signup'>Create an account</Link>
-                    </div>
+            <div style={{ position: "relative", width: "100vw", height: "100vh", overflow: "hidden" }}>
+                {/* Bouncing Balls Canvas */}
+                <BouncingBalls />
+                <Link to='/'><img
+                    src={logo}
+                    alt="Logo"
+                    style={{ position: "absolute", top: "20px", left: "20px", width: "100px", height: "100px" }}
+                /></Link>
 
-                    <div className="signin-form">
-                        <h2 className="form-title">Log In</h2>
-                        <form method="POST" className="register-form" id="login-form">
-                            <div className="form-group">
-                                 <label htmlFor="email"><EmailIcon /></label>
-                                <input type="email"  onChange={(e) => setUserDetails({...userDetails, email: e.target.value})} name="your_name" id="your_name" placeholder="Your Email"/>
+                {/* Signi Form */}
+                <div className='freedo'>
+
+                    <section className="signin">
+                        <div className="container">
+                            <div className="signin-content">
+                                <div className="signin-image">
+                                    <figure><img src={logor} alt="sing up image" /></figure>
+                                    <Link className="signup-image-link" to='/signup'>Create an account</Link>
+                                </div>
+
+                                <div className="signin-form">
+                                    <h2 className="form-title">Log In</h2>
+                                    <form method="POST" className="register-form" id="login-form">
+                                        <div className="form-group">
+                                            <label htmlFor="email"><EmailIcon /></label>
+                                            <input type="email" onChange={(e) => setUserDetails({ ...userDetails, email: e.target.value })} name="your_name" id="your_name" placeholder="Your Email" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="pass"><LockIcon /></label>
+                                            <input type="password" onChange={(e) => setUserDetails({ ...userDetails, Password: e.target.value })} name="your_pass" id="your_pass" placeholder="Password" />
+                                        </div>
+                                        <div className="form-group">
+                                            <input type="checkbox" name="remember-me" id="remember-me" className="agree-term" />
+                                            <label for="remember-me" className="label-agree-term"><span><span></span></span>Remember me</label>
+                                        </div>
+                                        <div className="form-group form-button">
+                                            <div className='mly' >
+                                                <button style={{
+                                                    backgroundColor: "gold",
+                                                    color: "black",
+                                                    fontSize: "1rem",
+                                                    padding: "10px 20px",
+                                                    border: "none",
+                                                    borderRadius: "5px",
+                                                    cursor: "pointer",
+                                                    fontWeight: "bold"
+                                                }} onClick={LoginCustomer} type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-lg" disabled={loading}>
+                                                    {loading ? (
+                                                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                                    ) : (
+                                                        "Login"
+                                                    )}</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <div className="social-login">
+                                        <span className="social-label">Or login with</span>
+                                        <ul className="socials">
+                                            <li><a href="#"><i className="display-flex-center zmdi zmdi-facebook"></i></a></li>
+                                            <li><a href="#"><i className="display-flex-center zmdi zmdi-twitter"></i></a></li>
+                                            <li><a href="#"><i className="display-flex-center zmdi zmdi-google"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="pass"><LockIcon /></label>
-                                <input type="password" onChange={(e) => setUserDetails({...userDetails, Password: e.target.value})} name="your_pass" id="your_pass" placeholder="Password"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="checkbox" name="remember-me" id="remember-me" className="agree-term" />
-                                <label for="remember-me" className="label-agree-term"><span><span></span></span>Remember me</label>
-                            </div>
-                            <div className="form-group form-button">
-                            <div className='mly' >
-                           <button style={{
-          backgroundColor: "gold",
-          color: "black",
-          fontSize: "1rem",
-          padding: "10px 20px",
-          border: "none",
-          borderRadius: "5px",
-          cursor: "pointer",
-          fontWeight: "bold"
-        }} onClick={LoginCustomer}  type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-lg"  disabled={loading}>
-             {loading ? (
-            <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-          ) : (
-            "Login"
-          )}</button>
-                               </div>
-                            </div>
-                        </form>
-                        <div className="social-login">
-                            <span className="social-label">Or login with</span>
-                            <ul className="socials">
-                                <li><a href="#"><i className="display-flex-center zmdi zmdi-facebook"></i></a></li>
-                                <li><a href="#"><i className="display-flex-center zmdi zmdi-twitter"></i></a></li>
-                                <li><a href="#"><i className="display-flex-center zmdi zmdi-google"></i></a></li>
-                            </ul>
                         </div>
-                    </div>
+                    </section>
+
                 </div>
             </div>
-        </section>
-      
-    </div>
-  </div>
 
-            
+
         </div>
     );
 };
@@ -137,44 +136,3 @@ const Login = () => {
 export default Login;
 
 
-{/* <form>
-      <section className="">
-                <div className="">
-                    <div className="">
-
-                        <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                            <form>
-                                <div className="form-outline mb-4">
-                                    <input 
-                                        type="email" 
-                                        className="form-control form-control-lg" 
-                                        placeholder="Enter a valid email address"
-                                        onChange={(e) => setUserDetails({...userDetails, email: e.target.value})}
-                                    />
-                                    <label className="form-label">Email address</label>
-                                </div>
-
-                                <div className="form-outline mb-3">
-                                    <input 
-                                        type="password" 
-                                        className="form-control form-control-lg" 
-                                        placeholder="Enter password"
-                                        onChange={(e) => setUserDetails({...userDetails, Password: e.target.value})}
-                                    />
-                                    <label className="form-label">Password</label>
-                                </div>
-
-                                <div className="text-center text-lg-start mt-4 pt-2">
-                                    <button type="button" className="btn btn-primary btn-lg" onClick={LoginCustomer}>
-                                        Login
-                                    </button>
-                                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? 
-                                        <Link to='/'>Register</Link>
-                                    </p>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </section>
-      </form> */}
